4f2a51d2876df08c3a4a55aa192c01e2b1113b8a
JuanQrz <juan.quirozors@gmail.com>
2021-04-24 14:13:44 -0500

    practica 6, y revinculacion de repsitorio

---
+++ Practica_6/practoca_6nmap.sh
@@ -0,0 +1,6 @@
+nmap 192.168.1.* >> ~/Escritorio/Respuesta.txt
+#############
+nmap -p 143,993 --script imap-brute scanme.nmap.org >> ~/Escritorio/Respuesta.txt
+#############
+nmap 189.152.177.252 >> ~/Escritorio/Respuesta.txt
+
